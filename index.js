4b04e35c0e3873affbb185e6a25d383224f7f309
+ <+>
2022-11-16 11:21:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+
+function add(a,b) {
+return a += b;
+}
+add(2,4);
+
+function subtract(a,b){
+return a -= b;
+}
+subtract(18,9); // number on the left  subtract the one on the right 
+
+function multiply(a,b){
+return a *= b;
+}
+multiply(8,9);
+
+
+function divide(a, b){
+  return a /= b;
+}
+divide(18,9);
+
+function increment(n){
+  return n+=1;
+}
+increment(18);
+
+function decrement(n){
+  return n-=1;
+}
+decrement(9);
+
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+makeInt("100");
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+  preserveDecimal("3.8979");
+  
+
+
+
+
+
+
+
